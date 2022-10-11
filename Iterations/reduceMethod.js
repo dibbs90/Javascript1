0fac894abdcc1ac14f37ae22f8e0e52df5901f69
Dibyendu <dibbsvcanary@gmail.com>
2022-10-11 17:19:31 +0530

    Create reduceMethod.js

---
+++ Iterations/reduceMethod.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 4, 10];
+ 
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+})
+ 
+console.log(summedNums) // Output: 17
+
+/*--------------------------------------*/
+
